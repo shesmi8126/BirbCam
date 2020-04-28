a640e38a3ca6bc9c52b8141fc2a221a30dcad181
bls220 <bls220@vt.edu>
2020-04-28 10:06:01 -0400

    Added stream and adjusted layout.

--- src/Component/App.js
+++ src/Component/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import './App.css';
-import { HashRouter, Route } from 'react-router-dom';
-import Header from './Header';
-import Main from './Main';
-import Photos from './Photos';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./Header";
+import Main from "./Main";
+import Photos from "./Photos";
 
 class App extends React.Component {
-
-    render() {
-      return (
-        <HashRouter>
-          <div className="App">
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
           <Header />
           <Route exact path="/" component={Main} />
           <Route exact path="/photos" component={Photos} />
-          </div>
-        </HashRouter>
-      );
-    }
+        </div>
+      </BrowserRouter>
+    );
   }
-  
-  export default App;
+}
+
+export default App;
--- src/Component/Main/index.js
+++ src/Component/Main/index.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import './Main.css';
-
+import React from "react";
+import JSMpeg from "@cycjimmy/jsmpeg-player";
+import "./Main.css";
 
 class Main extends React.Component {
-    render() {
-        return (
-            <main className="Main">
-                <div className="Main--content">
-                    <div className="Main--text">Birb Cam</div>
-                    <video height="800px"></video>
-                </div>
-            </main>
-        );
-    }
+  render() {
+    return (
+      <main className="Main">
+        <div className="Main--content">
+          <div className="Main--text">Birb Cam</div>
+          <div className="Main--stream">
+            <div className="jsmpeg"></div>
+          </div>
+        </div>
+      </main>
+    );
+  }
+
+  componentDidMount() {
+    new JSMpeg.VideoElement(".jsmpeg", "ws://subhobo.ddns.net:9999", {
+      autoplay: true,
+    });
+  }
 }
 
-
-export default Main
+export default Main;
